ea56203a4b4453ac28cd8d2536bfbf5c6f919096
mindlight9079 <78039377+mindlight9079@users.noreply.github.com>
2021-04-26 17:34:27 +0900

    자바1일차 : 주석 설명 예제

---
+++ java_kih/src/day1/HelloWorld.java
@@ -0,0 +1,17 @@
+package day1;
+
+public class HelloWorld {
+	
+	/**
+	 * 도큐먼트 주석
+	 */
+	
+	//주석
+	/* 작성일 : 2021-04-26
+	 * 작성자 : 경인혜
+	 * 내용 : 기본 프로젝트 생성 및 테스트*/
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
