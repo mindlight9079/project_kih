a9b008448a6e64895feb7ce37bcd0404a0308b7f
mindlight9079 <78039377+mindlight9079@users.noreply.github.com>
2021-04-26 17:37:08 +0900

    자바 1일차 : 연습문제

---
+++ java_kih/src/day1/Test1_2.java
@@ -0,0 +1,30 @@
+package day1;
+
+public class Test1_2 {
+
+	public static void main(String[] args) {
+		//정수 변수 num1에 1을 저장한 후 출력
+		int num1 = 1;
+		System.out.println(num1);
+		
+		// 정수 변수 num2에 2를 저장한 후 출력
+		int num2 = 2;
+		System.out.println(num2);
+		
+		// 문자 변수 op에 +를 저장한 후 출력
+		char op = '+';
+		//num1과 num2와 op를 조합하여 1+2가 출력되도록 작성해보세요.
+		/* 최종결과
+		 * 1
+		 * 2
+		 * +
+		 * 1+2
+		 * */		
+		 System.out.println(num1);
+		 System.out.println(num2);
+		 System.out.println(op);
+		 System.out.println(""+num1+op+num2);
+	
+	}
+
+}
