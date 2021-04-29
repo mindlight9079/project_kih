752c29280adb0a51740c80a3940122114fb35dee
mindlight9079 <78039377+mindlight9079@users.noreply.github.com>
2021-04-29 17:24:42 +0900

    자바4일차 : continue문

---
+++ java_kih/src/day4/Continue.java
@@ -0,0 +1,23 @@
+package day4;
+
+public class Continue {
+
+	public static void main(String[] args) {
+
+		int i;
+		for(i=1; i<=5 ; i++) {	
+			if( i == 2 ) {
+				continue;
+			}
+			System.out.println(i);			
+		}
+		
+		for(i=1; i<=5 ; i++) {	
+			if( i != 2 ) {
+				System.out.println(i);
+			}			
+		}
+		
+	}
+
+}
