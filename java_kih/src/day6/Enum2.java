eadb042ce8c3490985730eb0f8a867c9484b3482
boughpot <77330687+boughpot@users.noreply.github.com>
2021-05-03 17:35:21 +0900

    자바6일차 : Enum 예제

---
+++ java_kih/src/day6/Enum2.java
@@ -0,0 +1,53 @@
+package day6;
+
+import java.util.Scanner;
+
+public class Enum2 {
+
+	public static void main(String[] args) {
+
+		Week1 today = Week1.MONDAY;
+		System.out.println("요일을 입력하세요(1:월, 2:화, 3:수, 4:목, 5:금, 6: 토, 7:일) :");
+		Scanner scan = new Scanner(System.in);
+		int itoday = scan.nextInt();
+		today.setWeek(itoday);
+		
+		//열거형을 사용하고 요일을 입력하고 출력하는 예제
+		switch(today) {
+		case MONDAY: System.out.println("월요일입니다."); break;
+		case TUESDAY: System.out.println("화요일입니다."); break;
+		case WEDNESDAY: System.out.println("수요일입니다."); break;
+		case THURSDAY: System.out.println("목요일입니다."); break;
+		case FRIDAY: System.out.println("금요일입니다."); break;
+		case SATURDAY: System.out.println("토요일입니다."); break;
+		case SUNDAY: System.out.println("일요일입니다."); break;		
+		}
+		
+		
+//		scan.close();
+	}
+
+}
+enum Week1{
+	MONDAY(1), 
+	TUESDAY(2), 
+	WEDNESDAY(3), 
+	THURSDAY(4), 
+	FRIDAY(5), 
+	SATURDAY(6), 
+	SUNDAY(7);
+	
+	private int week;
+	
+	public int getWeek() {
+		return week;
+	}
+	
+	public void setWeek(int week) {
+		this.week = week;
+	}
+
+	Week1(int week){
+		this.week = week;
+	}
+}
