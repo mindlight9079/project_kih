7757d20e04a8ab72208ab8142e3464f9832b6f24
mindlight9079 <78039377+mindlight9079@users.noreply.github.com>
2021-06-24 17:46:04 +0900

    rolling.js

---
+++ 프론트/JS 수업/js/rolling.js
@@ -0,0 +1,20 @@
+function rollingTop(list, listItem, marginTop, animateTime, intervalTime){
+    var id = setInterval(function(){
+        if(!$(list+' '+listItem).first().is(':animated')){
+            $(list+' '+listItem).first().animate({'margin-top': marginTop+'px'},animateTime,function(){
+                $(this).detach().appendTo(list).removeAttr('style');
+            })
+        }
+    },intervalTime);
+    return id;
+}  
+
+function rollingLeft(list, listItem, animateTime, intervalTime){
+    var id =setInterval(function(){
+        var width =  $(list+' '+listItem).first().width();
+        $(list+' '+listItem).first().animate({'margin-left':'-'+width},animateTime,function(){
+            $(this).detach().appendTo(list).removeAttr('style');
+        })
+    },intervalTime)
+    return id;
+}
