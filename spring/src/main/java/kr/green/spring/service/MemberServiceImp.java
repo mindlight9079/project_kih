b4214ed8620dfc010cfe76656ce85f932791b9d3
mindlight9079 <78039377+mindlight9079@users.noreply.github.com>
2021-07-06 17:32:59 +0900

    SPRING : 회원 가입 및 게시글 리스트 구현 완료

--- spring/src/main/java/kr/green/spring/service/MemberServiceImp.java
+++ spring/src/main/java/kr/green/spring/service/MemberServiceImp.java
@@ -25,4 +25,16 @@ public class MemberServiceImp implements MemberService {
 		}
 		return dbUser;
 	}
+
+	@Override
+	public boolean signup(MemberVO user) {
+		//이미 가입된 아이디가 있으면 fales를 리턴
+		if(user == null || memberDao.getMember(user.getId()) != null) {
+			return false;
+		}	
+		//없으면 다오에게 회원 정보를 주면서 회원가입하라고 시킨 후 true를 리턴
+		memberDao.signup(user);
+			return true;
+		
+	}
 }
