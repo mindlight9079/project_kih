bc565b6205b811677733b737f2fb2c16b7210e13
mindlight9079 <78039377+mindlight9079@users.noreply.github.com>
2021-09-28 14:29:43 +0900

    PORTFOLIO : 카테고리 카트 수정

--- portfolio/src/main/java/kr/green/portfolio/dao/CartDAO.java
+++ portfolio/src/main/java/kr/green/portfolio/dao/CartDAO.java
@@ -30,7 +30,7 @@ public interface CartDAO {
 
 	ArrayList<CartVO> getPaymentList(@Param("isbn")BigInteger isbn, @Param("amount")Integer amount);
 
-	CartVO seletCartRegister(@Param("bigInteger") BigInteger bigInteger, String ca_me_id);
+	CartVO seletCartRegister(@Param("bigInteger") BigInteger bigInteger, @Param("ca_me_id") String ca_me_id);
 
 	void updateCartRegister(@Param("ca_num") int ca_num, @Param("integer") Integer integer);
 
